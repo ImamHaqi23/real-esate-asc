e97a0a65e8e8e47cffd422159ba97b09a5e61374
ImamBaihaqi <imam.baihaqi2396@gmail.com>
2024-02-24 10:15:52 +0700

    fix ant

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 /* Components */
-import { AntdRegistry } from '@ant-design/nextjs-registry';
 import { Providers } from '@/lib/providers';
 import './styles/globals.css';
 import Navbar from './components/Navbar/page';
@@ -10,11 +9,9 @@ export default function RootLayout(props: React.PropsWithChildren) {
     <Providers>
       <html lang="en">
         <body>
-          <AntdRegistry>
-            <Navbar />
-            {props.children}
-            <FooterComponent />
-          </AntdRegistry>
+          <Navbar />
+          {props.children}
+          <FooterComponent />
         </body>
       </html>
     </Providers>
